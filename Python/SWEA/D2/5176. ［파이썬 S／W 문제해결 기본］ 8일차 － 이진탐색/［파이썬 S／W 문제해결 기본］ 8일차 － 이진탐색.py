688fc602e7afc7fdbef746bc7f77706924d9f307
HJ <dlgmlwns25@naver.com>
2025-11-25 01:43:46 +0900

    [D2] Title: [파이썬 S/W 문제해결 기본] 8일차 - 이진탐색, Time: 66 ms, Memory: 56,192 KB -BaekjoonHub

---
+++ Python/SWEA/D2/5176. ［파이썬 S／W 문제해결 기본］ 8일차 － 이진탐색/［파이썬 S／W 문제해결 기본］ 8일차 － 이진탐색.py
@@ -0,0 +1,20 @@
+
+def inord(n):
+    global cnt
+    if n<=N:
+        inord(n*2)
+        lst[n] = cnt
+        cnt+=1
+        inord(n*2+1)
+
+T = int(input())
+for test_case in range(1, T + 1):
+    # 
+    
+    N = int(input())
+    
+    lst = [0] * (N+1)
+    
+    cnt = 1
+    inord(1)
+    print(f"#{test_case} {lst[1]} {lst[N//2]}")
