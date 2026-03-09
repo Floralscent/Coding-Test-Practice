e692436587eaee7a6a45d64e549ae33e33533a49
HJ <dlgmlwns25@naver.com>
2026-03-09 19:15:06 +0900

    [Gold V] Title: 컨베이어 벨트 위의 로봇, Time: 2808 ms, Memory: 34952 KB -BaekjoonHub

---
+++ Python/백준/Gold/20055. 컨베이어 벨트 위의 로봇/컨베이어 벨트 위의 로봇.py
@@ -0,0 +1,42 @@
+from collections import deque
+import sys
+
+n,k = map(int, (sys.stdin.readline().split()))
+
+li = list(map(int,(input().split())))
+
+belt = deque(li)
+robot = deque([0]*n)
+cnt = 1
+
+while True:
+    #
+    belt.rotate(1)
+    robot.rotate(1)
+
+    #
+    if robot[n-1] == 1:
+        robot[n-1] = 0
+
+
+    for i in range(n-1,-1,-1):
+        if belt[i+1] >= 1:
+                if robot[i] == 1 and robot[i+1] ==0 and belt[i+1]>0:
+                    robot[i] = 0
+                    robot[i+1] = 1
+                    belt[i+1] -=1
+    #
+    if robot[n-1] == 1:
+        robot[n-1] = 0
+
+    if belt[0] >0:
+        belt[0] -= 1
+        robot[0] =1
+    #
+    tmp = belt.count(0)
+    #
+    if tmp>=k:
+        print(f"{cnt}")
+        sys.exit(0)
+    else:
+        cnt+=1
